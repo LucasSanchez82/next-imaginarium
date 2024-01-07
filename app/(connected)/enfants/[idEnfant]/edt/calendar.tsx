f917f48393039a633baf27d5522bb75d6514b46d
Lucas <jiraya1008@gmail.com>
2024-01-07 22:11:18 +0100

    Refactor calendar component and add event modal
    This commit refactors the `Calendar` component in `calendar.tsx` to improve code organization and readability. It also adds functionality to handle event clicks and selection of date ranges. Additionally, the `AddEventModal` component in `addEventModal.tsx` has been updated to support updating events and display default values for title and description fields.

--- app/(connected)/enfants/[idEnfant]/edt/calendar.tsx
+++ app/(connected)/enfants/[idEnfant]/edt/calendar.tsx
@@ -1,44 +1,114 @@
 "use client";
-import React, { useState } from "react";
-import Fullcalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
+import { Button } from "@/components/ui/button";
 import { AddEventModal } from "@/components/ui/edt/addEventModal";
-import { EventDateType } from "@/components/types/event";
+import { useToast } from "@/components/ui/use-toast";
 import {
   DateSelectArg,
+  EventClickArg,
   EventInput,
-  EventSourceInput,
 } from "@fullcalendar/core/index.js";
-import { set } from "date-fns";
+import { EventImpl } from "@fullcalendar/core/internal";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
+import Fullcalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import { useState } from "react";
+
 
 function Calendar() {
-  // const [eventModal, setEventModal] = useState<EventDateType>();
-  const [showModal, setShowModal] = useState(false)
-  const [calendarEvents, setCalendarEvents] = useState<EventInput[]>([]);
-  const [currDate, setCurrDate] = useState<{start: Date, end?: Date}>({start: new Date()})
+  const [calendarState, setCalendarState] = useState<{
+    showModal: boolean;
+    calendarEvents: EventInput[];
+    currDate: { start: Date; end?: Date };
+    updateEvent: EventImpl | null;
+  }>({
+    showModal: false,
+    calendarEvents: [],
+    currDate: { start: new Date() },
+    updateEvent: null,
+  });
+  const { showModal, calendarEvents } = calendarState;
+  const { toast } = useToast();
 
+  /**
+   * Adds a new event to the calendar state.
+   * @param {EventInput} event - The event to be added.
+   */
   const addCalendarEvent = (event: EventInput) => {
-    setCalendarEvents((curr) => [...curr, {...event, ...currDate}]);
+    setCalendarState((curr) => ({
+      ...curr,
+      calendarEvents: [...curr.calendarEvents, {id: String(curr.calendarEvents.length+1), ...event, ...curr.currDate }],
+    }));
   };
 
-  // const setOpenEventModal = (open: boolean) => {
-  //   setCalendarEvents((curr) => ({ ...curr}));
-  // };
+  /**
+   * Sets the visibility of the modal.
+   * @param {boolean} show - The visibility state to be set.
+   */
+  const setShowModal = (show: boolean) => {
+    setCalendarState((curr) => ({
+      ...curr,
+      showModal: show,
+    }));
+  };
 
+  /**
+   * Updates an event in the calendar state.
+   * @param {EventInput} event - The event to be updated.
+   */
+  const setUpdateEvent = (event: EventImpl | null) => {
+    setCalendarState((curr) => ({ ...curr, updateEvent: event }));
+  };
+
+  /**
+   * Sets the current date in the calendar state.
+   * @param {{ start: Date; end?: Date }} date - The date to be set.
+   */
+  const setCurrDate = (date: { start: Date; end?: Date }) => {
+    setCalendarState((curr) => ({
+      ...curr,
+      currDate: date,
+    }));
+  };
+
+  /**
+   * Handles the click event on a date.
+   * @param {DateClickArg} event - The event object containing the clicked date.
+   */
   const handleDateClick = (event: DateClickArg) => {
-    setCurrDate(curr => ({start: event.date}))
-    setShowModal(true)
+    setCurrDate({ start: event.date });
+    setShowModal(true);
   };
+
+  /**
+   * Handles the click event on an event.
+   * @param {EventClickArg} event - The event object containing the clicked event.
+   */
+  const handleEventClick = ({ event, el, view }: EventClickArg) => {
+    setUpdateEvent(event);
+    if (event.end && event.start) {
+      setCurrDate({ start: event.start, end: event.end });
+    }else {
+      toast({
+        title: "Erreur",
+        description: "L'évènement n'a pas de dates.",
+      })
+    }
+    setShowModal(true);
+  };
+
+  /**
+   * Handles the selection of a date range.
+   * @param {DateSelectArg} args - The object containing the start and end dates of the selection.
+   */
   const handleSelect = (args: DateSelectArg) => {
-    const { currentEnd, currentStart, } = args.view;
-    
-    setCurrDate({start: args.start, end: args.end})
-    setShowModal(true)
+    setCurrDate({ start: args.start, end: args.end });
+    setShowModal(true);
   };
+
   return (
     <div>
+      <Button onClick={() => console.log(calendarEvents)}>voir les events</Button>
       <Fullcalendar
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
         initialView={"dayGridMonth"}
@@ -48,16 +118,26 @@ function Calendar() {
           end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
         }}
         height={"90vh"}
+        // eventContent={({event}) => (
+        //   <div className="flex flex-col justify-start items-center m-auto bg-blue-400 rounded p-1">
+        //     <b>{event.title}</b>
+        //     <i className="">{event._def.extendedProps.description}</i>
+        //   </div>
+        // )}
         dateClick={handleDateClick}
         selectable={true}
         select={handleSelect} // triggered when a date/time selection is made;
         events={calendarEvents}
+        eventClick={handleEventClick}
       />
       <AddEventModal
-        // date={eventModal.date}
         open={showModal}
         setOpen={setShowModal}
         addCalendarEvent={addCalendarEvent}
+        useUpdateEvent={{
+          updateEvent: calendarState.updateEvent,
+          updateEventReset: () => setUpdateEvent(null),
+        }}
       />
     </div>
   );
--- components/ui/edt/addEventModal.tsx
+++ components/ui/edt/addEventModal.tsx
@@ -1,27 +1,44 @@
 import { Button } from "@/components/ui/button";
 import {
-    Dialog,
-    DialogContent,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
 } from "@/components/ui/dialog";
 import eventSchema from "@/components/zodSchemas/event";
 import { EventInput } from "@fullcalendar/core/index.js";
+import { EventImpl } from "@fullcalendar/core/internal";
 import AutoForm from "../auto-form";
+import { use, useEffect } from "react";
 
 export function AddEventModal({
   open,
   setOpen,
   addCalendarEvent,
+  useUpdateEvent,
 }: {
   open: boolean;
   setOpen: (open: boolean) => void;
-  addCalendarEvent: (event: EventInput) => void
+  addCalendarEvent: (event: EventInput) => void;
+
+  useUpdateEvent: {
+    updateEvent: EventImpl | null;
+    updateEventReset: () => void;
+  };
 }) {
+  const { updateEvent, updateEventReset } = useUpdateEvent;
+
+  useEffect(() => {
+    if (!open) {
+      console.log("ferme");
+      updateEventReset();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [open]);
+
   const handleSubmit = (values: { title: string; description: string }) => {
-    // console.log({ ...values, date: date.toLocaleString() });
-    addCalendarEvent({...values})
+    addCalendarEvent({ ...values });
     setOpen(false);
   };
 
@@ -36,7 +53,28 @@ export function AddEventModal({
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
         </DialogHeader>
-        <AutoForm formSchema={eventSchema} onSubmit={handleSubmit}>
+        <AutoForm
+          formSchema={eventSchema}
+          onSubmit={handleSubmit}
+          fieldConfig={{
+            title: {
+              description: "The title of the event.",
+              fieldType: "fallback",
+              inputProps: {
+                placeholder: "Mon titre...",
+                defaultValue: updateEvent?._def.title,
+              },
+            },
+            description: {
+              description: "The description of the event.",
+              fieldType: "textarea",
+              inputProps: {
+                placeholder: "Ma description...",
+                defaultValue: updateEvent?._def.extendedProps.description,
+              },
+            },
+          }}
+        >
           <DialogFooter>
             <Button type="submit">Save changes</Button>
           </DialogFooter>
