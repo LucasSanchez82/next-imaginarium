67184efd0d623725d01cb29381be62b8288dc891
Lucas <jiraya1008@gmail.com>
2024-02-14 00:08:10 +0100

    Refactor form.tsx to remove unused imports and commented code

--- app/(connected)/categorie/form.tsx
+++ app/(connected)/categorie/form.tsx
@@ -1,17 +1,15 @@
 "use client";
 
 import { addCategorieToDb } from "@/components/actions/categorie";
-import AutoForm from "@/components/ui/auto-form";
-import { Button } from "@/components/ui/button";
-import { categorieSchema } from "./zodSchemas";
-import { useToast } from "@/components/ui/use-toast";
 import { SubmitButton } from "@/components/submitButton";
+import AutoForm from "@/components/ui/auto-form";
+import { useToast } from "@/components/ui/use-toast";
+import { categorieSchema } from "./zodSchemas";
 
 const FormCategorie = ({ className }: { className?: string }) => {
   const { toast } = useToast();
   return (
     <AutoForm
-      // action={addCategorieToDb}
       parsedAction={async (values) => {
         try {
           await addCategorieToDb(values);
